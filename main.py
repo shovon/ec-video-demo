9ae37beda93ade83b2b3e7ad3a7cf26f2ef16619
Sal Rahman <salehen.rahman@gmail.com>
2022-08-30 14:01:10 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,172 @@
+from manim import *
+
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
+
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        # interpolate the square into the circle
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))  # fade out animation
+
+
+class SquareAndCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+
+        square = Square()  # create a square
+        square.set_fill(BLUE, opacity=0.5)  # set the color and transparency
+
+        square.next_to(circle, RIGHT, buff=0.5)  # set the position
+        self.play(Create(circle), Create(square))  # show the shapes on screen
+
+
+class AnimatedSquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        square = Square()  # create a square
+
+        self.play(Create(square))  # show the square on screen
+        self.play(square.animate.rotate(PI / 4))  # rotate the square
+        self.play(
+            ReplacementTransform(square, circle)
+        )  # transform the square into a circle
+        self.play(
+            circle.animate.set_fill(PINK, opacity=0.5)
+        )  # color the circle on screen
+
+
+class BraceAnnotation(Scene):
+    def construct(self):
+        dot = Dot([-2, -1, 0])
+        dot2 = Dot([2, 1, 0])
+        line = Line(dot.get_center(), dot2.get_center()).set_color(ORANGE)
+        b1 = Brace(line)
+        b1text = b1.get_text("Horizontal distance")
+        b2 = Brace(line, direction=line.copy().rotate(
+            PI / 2).get_unit_vector())
+        b2text = b2.get_tex("x-x_1")
+        self.add(line, dot, dot2, b1, b2, b1text, b2text)
+
+
+class MovingFrameBox(Scene):
+    def construct(self):
+        text = MathTex(
+            "\\frac{d}{dx}f(x)g(x)=", "f(x)\\frac{d}{dx}g(x)", "+",
+            "g(x)\\frac{d}{dx}f(x)"
+        )
+        self.play(Write(text))
+        framebox1 = SurroundingRectangle(text[1], buff=.1)
+        framebox2 = SurroundingRectangle(text[3], buff=.1)
+        self.play(
+            Create(framebox1),
+        )
+        self.wait()
+        self.play(
+            ReplacementTransform(framebox1, framebox2),
+        )
+        self.wait()
+
+
+class EquationOfTheLine(Scene):
+    def construct(self):
+        g = VGroup()
+
+        p1 = MathTex("P_1 = (x_1, x_2)")
+        p2 = MathTex("P_2 = (x_2, y_2)")
+        eq1 = MathTex(
+            "y = ", "m", "x + d"
+        )
+
+        g += p1
+        g += p2
+        g += eq1
+
+        self.play(g)
+
+
+class EllipticCurveEquation(Scene):
+    def construct(self):
+        text = MathTex(
+            "y^2=x^3+ax+b"
+        )
+        self.play(Write(text))
+
+
+class SinAndCosFunctionPlot(Scene):
+    def construct(self):
+        axes = Axes(
+            x_range=[-10, 10.3, 1],
+            y_range=[-1.5, 1.5, 1],
+            x_length=10,
+            axis_config={"color": GREEN},
+            x_axis_config={
+                "numbers_to_include": np.arange(-10, 10.01, 2),
+                "numbers_with_elongated_ticks": np.arange(-10, 10.01, 2),
+            },
+            tips=False,
+        )
+        axes_labels = axes.get_axis_labels()
+        sin_graph = axes.plot(lambda x: np.sin(x), color=BLUE)
+        cos_graph = axes.plot(lambda x: np.cos(x), color=RED)
+
+        sin_label = axes.get_graph_label(
+            sin_graph, "\\sin(x)", x_val=-10, direction=UP / 2
+        )
+        cos_label = axes.get_graph_label(cos_graph, label="\\cos(x)")
+
+        vert_line = axes.get_vertical_line(
+            axes.i2gp(TAU, cos_graph), color=YELLOW, line_func=Line
+        )
+        line_label = axes.get_graph_label(
+            cos_graph, "x=2\pi", x_val=TAU, direction=UR, color=WHITE
+        )
+
+        plot = VGroup(axes, sin_graph, cos_graph, vert_line)
+        labels = VGroup(axes_labels, sin_label, cos_label, line_label)
+        self.add(plot, labels)
+
+
+def func_to_graph2(self, x):
+
+    return np.sin(x)
+
+
+class ArgMinExample(Scene):
+    def construct(self):
+        ax = Axes(
+            x_range=[0, 10], y_range=[0, 100, 10], axis_config={"include_tip": False}
+        )
+        labels = ax.get_axis_labels(x_label="x", y_label="f(x)")
+
+        t = ValueTracker(0)
+
+        def func(x):
+            return 2 * (x - 5) ** 2
+        graph = ax.plot(func, color=MAROON)
+
+        initial_point = [ax.coords_to_point(
+            t.get_value(), func(t.get_value()))]
+        dot = Dot(point=initial_point)
+
+        dot.add_updater(lambda x: x.move_to(
+            ax.c2p(t.get_value(), func(t.get_value()))))
+        x_space = np.linspace(*ax.x_range[:2], 200)
+        minimum_index = func(x_space).argmin()
+
+        self.add(ax, labels, graph, dot)
+        self.play(t.animate.set_value(x_space[minimum_index]))
+        self.wait()
